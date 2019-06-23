9688100655aa0009e87f1587c6882d9e9b947198
jsycdut <jsycdut@gmail.com>
2019-06-23 15:24:35 +0800

    feat: create shell preference configurations

---
+++ shell/shell_pref.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -e
+
+# alias
+alias e="vim"
+alias ss="sudo systemctl"
+alias stop="sudo systemctl stop"
+alias start="sudo systemctl start"
+alias status="sudo systemctl status"
+alias restart="sudo systemctl restart"
+
+# install sdkman and java scala sbt kotlin groovy gradle maven
+curl -s "https://get.sdkman.io" | bash
+sdk install java
+sdk install scala
+sdk install kotlin
+sdk install groovy
+sdk instann gradle
+sdk instann maven
+sdk instann sbt
