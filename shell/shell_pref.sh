73ca3e04817fbd07cb309c56e8698743e1f4da06
jsycdut <jsycdut@gmail.com>
2019-07-07 12:51:51 +0800

    fix: install sdk software in subshell

--- shell/shell_pref.sh
+++ shell/shell_pref.sh
@@ -8,12 +8,16 @@ alias start="sudo systemctl start"
 alias status="sudo systemctl status"
 alias restart="sudo systemctl restart"
 
-# install sdkman and java scala sbt kotlin groovy gradle maven
+# install sdkman and java scala sbt kotlin groovy gradle maven in subshell
 curl -s "https://get.sdkman.io" | bash
-sdk install java
-sdk install scala
-sdk install kotlin
-sdk install groovy
-sdk instann gradle
-sdk instann maven
-sdk instann sbt
+
+(
+  sdk install java
+  sdk install scala
+  sdk install kotlin
+  sdk install groovy
+  sdk instann gradle
+  sdk instann maven
+  sdk instann sbt
+)
+
