82ff23884ef03ff09dc288019790a36c4893c356
jsycdut <jsycdut@gmail.com>
2019-07-07 15:57:30 +0800

    feat: v2ray client scripts

---
+++ shell/v2ray/client/v2ray-client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE_DIR=`pwd`
+pm=""
+
+command -v apt > /dev/null 2 >& 1 && pm="apt"
+command -v yum > /dev/null 2 >& 1 && pm="yum"
+command -v pacman > /dev/null 2 >& 1 && pm="pacman"
+
+if [[ `id -u` != 0 ]]; then
+  echo "This script neede to be executed as root"
+  exit -1
+fi
+
+$pm install -y v2ray
+
+cp `command -v  v2ray` /usr/bin
+cp `command -v v2rayctl` /usr/bin
+
+mkdir /etc/v2ray
+cp $BASE_DIR/config.json /etc/v2ray
+
+cp $BASE_DIR/v2ray-client.service /lib/systemd/system
