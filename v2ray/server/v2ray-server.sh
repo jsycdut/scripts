5ca19c6a12f6a177f9d0b983973ec661dc927954
jsycdut <jsycdut@gmail.com>
2019-07-09 10:14:15 +0800

    feat: add more prompt to server script

--- v2ray/server/v2ray-server.sh
+++ v2ray/server/v2ray-server.sh
@@ -27,8 +27,8 @@ fi
 ## Arch check
 echo "=====================Downloading Essential Files================="
 
-mkdir /v2ray
 V2RAY_HOME=/v2ray
+mkdir -p $V2RAY_HOME/config
 
 case $ARCH in
   x86_64)
@@ -39,24 +39,33 @@ case $ARCH in
     ;;
 esac
 
-tar -xf $V2RAY_HOME/go.tar.gz -C $V2RAY_HOME
-export PATH=$PATH:$V2RAY_HOME/go/bin
+tar -xf go.tar.gz -C $V2RAY_HOME
 
+export PATH=$PATH:$V2RAY_HOME/go/bin
 export GOPATH=$V2RAY_HOME
 
 echo "=======================   Building v2ray   ====================="
 echo "Getting v2ray source files..."
 go get -u v2ray.com/core/...
+
 echo "Building v2ray..."
 go build -o $V2RAY_HOME/bin/v2ray v2ray.com/core/main
+echo "Building v2ray finished: $V2RAY_HOME/bin/v2ray"
+
 echo "Building v2ctl..."
 go build -o $V2RAY_HOME/bin/v2ctl v2ray.com/core/infra/control/main
+echo "Building v2ctl finished: $V2RAY_HOME/bin/v2ctl"
 
 echo "=======================  Configuring v2ray ====================="
-mkdir $V2RAY_HOME/config && cp $BASE_DIR/config.json $V2RAY_HOME/config
-cp v2ray-server.service /lib/systemd/system
-cp `find $V2RAY_HOME -name 'geoip.dat'` $V2RAY_HOME/bin
-cp `find $V2RAY_HOME -name 'geosite.dat'` $V2RAY_HOME/bin
+cp $BASE_DIR/config.json $V2RAY_HOME/config
+cp $BASE_DIR/v2ray-server.service /lib/systemd/system
+
+cp `find $GOPATH -name 'geoip.dat'` $V2RAY_HOME/bin
+cp `find $GOPATH -name 'geosite.dat'` $V2RAY_HOME/bin
+
+# TODO: BBR probe and apply
 
 # Start v2ray
 systemctl start v2ray-server.service
+
+echo "Finished, go to $V2RAY_HOME/config/config.json for your v2ray configuration details, see you"
