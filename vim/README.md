889d7bb4876006ad26be887959b2ee8b470855cf
jsycdut <jsycdut@gmail.com>
2020-02-29 09:48:34 +0800

    feat: update vim/install.sh

--- vim/README.md
+++ vim/README.md
@@ -1,6 +1,2 @@
-# README
-
-jsy's personal vim config file.
-
-## 效果截图
-![pic not found](https://raw.githubusercontent.com/jsycdut/photos/master/vim/vim-screen.png)
+# scripts
+just some scripts
--- vim/install.sh
+++ vim/install.sh
@@ -9,6 +9,10 @@ mkdir -p $NVIM_CONFIG_DIR/autoload
 curl -fLo $NVIM_CONFIG_DIR/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# clear
+test -e $HOME/.vimrc && rm $HOME/.vimrc
+test -e $NVIM_CONFIG_DIR/init.vim && rm $NVIM_CONFIG_DIR/init.vim
+
 # link config files
 cd $NVIM_CONFIG_DIR
 ln -s ${CURRENT_DIR}/vimrc init.vim
@@ -16,10 +20,9 @@ ln -s ${CURRENT_DIR}/vimrc init.vim
 cd $HOME
 ln -s ${CURRENT_DIR}/vimrc .vimrc
 
-# get and install font 
-mkdir -p  $FONT_DIR
-cd $FONT_DIR
-wget -O ubuntu-nerd-mono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
-unzip ubuntu-nerd-mono.zip
-fc-cache -fv
-
+## get and install font
+#mkdir -p  $FONT_DIR
+#cd $FONT_DIR
+#wget -O ubuntu-nerd-mono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
+#unzip ubuntu-nerd-mono.zip
+#fc-cache -fv
